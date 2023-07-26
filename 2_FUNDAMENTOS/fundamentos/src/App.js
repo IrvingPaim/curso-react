885e1c2b396088df618f827e8e6c82108f729414
Irving Paim <irvingpaim@yahoo.com.br>
2023-07-26 11:57:31 -0300

    atualização

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+//components
+import FirstComponent from './components/FirstComponent'
+
+// style.css
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,7 @@
+const FirstComponent = () => {
+    return (
+        <h1>Meu Primeiro Componente</h1>
+    )
+}
+
+export default FirstComponent
