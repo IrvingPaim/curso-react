263d7faf8b28e6316ecaaaace371f855717e91c2
Irving Paim <irvingpaim@yahoo.com.br>
2023-07-27 15:19:32 -0300

    Finalizado seção 2 do curso de react

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/Challenge.js
@@ -0,0 +1,12 @@
+const Challenge = () => {
+    const x = 2
+    const y = 10
+
+    return (
+        <div>
+            <button onClick={() => console.log(x+y)}>Somar números</button>
+        </div>
+    )
+}
+
+export default Challenge
