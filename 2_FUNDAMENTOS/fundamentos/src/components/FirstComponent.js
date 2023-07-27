58d1cf3313d6f63619c3a356f229e0c55a44631a
Irving Paim <irvingpaim@yahoo.com.br>
2023-07-26 21:02:33 -0300

    Template Expressions

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,6 +1,13 @@
+// arquivo de estilo
+
 const FirstComponent = () => {
+// essa função faz isso
     return (
-        <h1>Meu Primeiro Componente</h1>
+        <div>
+            {/* Algum comentário */}
+            <h1>Meu Primeiro Componente</h1>
+            <p className="teste">Meu Texto</p>
+        </div>  
     )
 }
 
