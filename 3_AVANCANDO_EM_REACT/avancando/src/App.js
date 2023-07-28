dba7c3a8f1dde3e91c0045296bc8b30c11f77bbf
Irving Paim <irvingpaim@yahoo.com.br>
2023-07-28 16:49:36 -0300

    Imagens em react

---
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/*Imagem em public*/}
+      <div>
+        <img src="/img1.jpg" alt="paisagem" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
