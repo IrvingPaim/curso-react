1f0640a99ae35e38f4bb888dc9dbaefc869926e4
Irving Paim <irvingpaim@yahoo.com.br>
2023-08-04 20:22:29 -0300

    teste

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,14 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Avançando em React</h1>
-      {/*Imagem em public*/}
-      <div>
-        <img src="/img1.jpg" alt="paisagem" />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
