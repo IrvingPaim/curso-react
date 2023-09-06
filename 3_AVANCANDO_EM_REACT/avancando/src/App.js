4e8a242677a3043ac5f2ed629f9cc6a19f2449ae
Irving Paim <irvingpaim@yahoo.com.br>
2023-09-06 20:10:44 -0300

    Atualizado até props

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 import City from "./assets/city.jpg"
 import CondicionalRender from './components/CondicionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
 
 function App() {
+  const name = "Irving"
+
+  const [userName] = useState("Irving Paim")
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -20,6 +27,13 @@ function App() {
       <ManageData />
       <ListRender />
       <CondicionalRender />
+      {/* props */}
+      <ShowUserName name={userName} />
+      {/* destructuring */}
+      <CarDetails brand="VW" km={100000} color="Azul" newCar={false} />
+      {/* reaproveitando */}
+      <CarDetails brand="Ford" km={0} color="Vermelho" newCar={true} />
+      <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false} />
     </div>
   );
 }
