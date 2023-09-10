f2e8a007556c13e75b2b6fe317102c1f5c3a8902
Irving Paim <irvingpaim@yahoo.com.br>
2023-09-09 21:08:29 -0300

    Iniciando aulas do módulo 4 de react

---
+++ 4_CSS_REACT/css/src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './App.css';
+import Mycomponent from './components/MyComponent';
+
+function App() {
+  const n = 9
+
+  const [name] = useState("Irving")
+
+  return (
+    <div className="App">
+      {/* CSS global */}
+      <h1>React com CSS</h1>
+      {/* CSS de componente */}
+      <Mycomponent />
+      <p>Este parágrafo é do app.js</p>
+      {/* Inline CSS */}
+      <p style={{color: "blue", padding: "25px", borderTop: "5px solid red"}}>Este elemento foi estilizado de forma inline</p>
+      {/* Inline CSS dinâmico */}
+      <h2 style={n < 10 ? ({color: "purple"}) : ({color: "blue"})}>Css dinâmico</h2>
+      <h2 style={n > 10 ? ({color: "purple"}) : ({color: "blue"})}>Css dinâmico</h2>
+      <h2 style={name === "Irving" ? ({color: "blue"}) : ({color: "purple"})}>{name}</h2>
+      <h2 style={name !== "Irving" ? ({color: "blue"}) : ({color: "purple"})}>{name}</h2>
+    </div>
+  );
+}
+
+export default App;
