83be8aa698a80e69dcc245106ef5f4bae925a273
Irving Paim <irvingpaim@yahoo.com.br>
2023-10-02 21:24:04 -0300

    Avançando no módulo 7 react

---
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+import { useState, useEffect } from "react"
+
+const url = "http://localhost:3000/products"
+
+function App() {
+  const [products, setProducts] = useState([])
+
+  // 1 - Resgatando Dados
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url)
+
+      const data = await response.json()
+
+      setProducts(data)
+    }
+
+    fetchData()
+
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>  
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
